e8f6c8e082c15eaf772cab6ab60476e5ff59e6f7
Mihirxdhruv <devidgatta2000@gmail.com>
2024-07-08 21:50:12 +0530

    Update m.py

--- m.py
+++ m.py
@@ -153,7 +153,7 @@ def add_user(message):
         else:
             response = "Please specify a user ID and the duration (e.g., 1hour, 2days, 3weeks, 4months) to add 😘."
     else:
-        response = "You have not purchased yet purchase now from:- @venomXcrazy."
+        response = "You have not purchased yet purchase now from:- @msd."
 
     bot.reply_to(message, response)
 
